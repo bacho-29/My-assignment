0e23587ffeec2ac1c154a1b13f05b5ea37e52e0d
bacho-29 <bachoarishvili@gmail.com>
2025-02-20 14:55:52 +0400

    Add files via upload

---
+++ I Essignment.py
@@ -0,0 +1,38 @@
+# 1 work
+first_name = "Bacho"
+last_name = "Arishvili"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+print(type(full_name))
+
+# 2 work
+age = 28
+
+print(f"You are {age} years old.")
+print(type(age))
+
+# 3 work
+temperature = 21
+
+print(f"The temperature is {temperature} degrees.")
+print(type(temperature))
+
+# 4 work
+temperature = 25
+is_hot = temperature > 30
+
+print(is_hot)
+print(type(is_hot))
+
+# 5 work
+int_example = 7
+flo_example = 7.9
+str_example = "Hello, Earth!"
+bool_example = True
+
+print(f"Integer example: {int_example}, Type: {type(int_example)}")
+print(f"Float example: {flo_example}, Type: {type(flo_example)}")
+print(f"String example: {str_example}, Type: {type(str_example)}")
+print(f"Boolean example: {bool_example}, Type: {type(bool_example)}")
