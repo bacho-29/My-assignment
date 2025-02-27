3b85cdee4bc75f1cace5996dd6267a5db6f9a67b
bacho-29 <bachoarishvili@gmail.com>
2025-02-27 18:18:18 +0400

    Create II Assignment.py

---
+++ II Assignment.py
@@ -0,0 +1,72 @@
+message = "Hello Customers."
+print(message)
+
+# Exercise 2
+message = "News on the market."
+print(message)
+message = "News on the platform."
+print(message)
+
+# Exercise 3
+person_name = "Einstein"
+print(person_name.lower())     
+print(person_name.upper())     
+print(person_name.title())      
+
+# Exercise 4
+print("\tMy favorite food:")
+print("\tKhinkali")
+print("\tBurger")
+print("\tPizza")
+
+# Exercise 8
+whitespace_string = '          I have white spaces                '
+print(whitespace_string.strip())
+
+# Exercise 9
+left_trimmed_string = whitespace_string.lstrip()
+print(left_trimmed_string)
+
+# Exercise 10
+right_trimmed_string = whitespace_string.rstrip()
+print(right_trimmed_string)
+
+# Exercise 11
+single_quote_string = 'One of a dog\'s greatest advanteges is its sense of smell.'
+print(single_quote_string)
+
+# Exercise 12
+person_name = "Bacho"
+print(f"Hello {person_name}, are you still lazy in python?")
+
+# Exercise 13
+print('ბერნარდ შოუმ ერთხელ თქვა, "ვინც იცის, აკეთებს, ვინც არა - სხვას ასწავლის."')
+
+# Exercise 14
+famous_person = "ბერნარდ შოუმ"
+message = f'{famous_person} ერთხელ თქვა, "ვინც იცის, აკეთებს, ვინც არა - სხვას ასწავლის."'
+print(message)
+
+# Exercise 15
+print(4 + 4)  
+print(12 - 4) 
+print(8 * 1)  
+print(24 / 3) 
+
+# Exercise 16
+favorite_number = 29
+print(f"My favorite number is {favorite_number}.")
+
+# Exercise 17
+
+# Famous person and their quote
+famous_person = "ბერნარდ შოუმ"
+quote = "ვინც იცის, აკეთებს, ვინც არა - სხვას ასწავლის."
+message = f'{famous_person} ერთხელ თქვა, {quote}'
+print(message)
+
+# Arithmetic operations that result in the number 8
+print(4 + 4)  
+print(12 - 4) 
+print(8 * 1)  
+print(24 / 3)
